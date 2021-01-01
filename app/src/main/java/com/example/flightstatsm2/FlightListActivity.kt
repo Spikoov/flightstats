f577364aa30c82f1898ffee2159f4be1d7ad2ab6
Spikoov <hem.jyh@gmail.com>
2021-01-01 14:26:06 +0100

    switch fragment (map to flight details)

--- app/src/main/java/com/example/flightstatsm2/FlightListActivity.kt
+++ app/src/main/java/com/example/flightstatsm2/FlightListActivity.kt
@@ -1,7 +1,10 @@
 package com.example.flightstatsm2
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_flight_list.*
@@ -15,8 +18,6 @@ class FlightListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flight_list)
 
-        val isMobile = detail_container == null
-
         viewModel = ViewModelProvider(this).get(FlightListViewModel::class.java)
         viewModel.search(
             intent.getStringExtra("icao")!!,
@@ -26,23 +27,34 @@ class FlightListActivity : AppCompatActivity() {
         )
 
         viewModel.getSelectedFlightNameLiveData().observe(this, {
-            //switch fragment
-            //val newFragment: FlightDetailFragment = FlightDetailFragment.newInstance()
-            val newFragment: MapFragment = MapFragment.newInstance()
-            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-            if (isMobile) {
-                transaction.add(R.id.activityContainer, newFragment)
-                transaction.addToBackStack(null)
-
-                transaction.commit()
-            }
-            else{
-                transaction.add(R.id.detail_container, newFragment)
-                transaction.addToBackStack(null)
-
-                transaction.commit()
-            }
+            val mapFragment: MapFragment = MapFragment.newInstance()
+            switchFragment(mapFragment)
         })
 
+    }
+
+    fun switchFragment(newFragment : Fragment){
+        val isMobile = detail_container == null
+
+        //switch fragment
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        if (isMobile) {
+            transaction.add(R.id.activityContainer, newFragment)
+            transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
+        else{
+            transaction.add(R.id.detail_container, newFragment)
+            transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
+    }
+
+    fun onDetailsButtonClick(view: View) {
+        //change fragment : map to details
+        val flightDetailFragment: FlightDetailFragment = FlightDetailFragment.newInstance()
+        switchFragment(flightDetailFragment)
     }
 }
--- app/src/main/java/com/example/flightstatsm2/MapFragment.kt
+++ app/src/main/java/com/example/flightstatsm2/MapFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
